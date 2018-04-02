4879060ed63ffd933c563e443144c7b9fca4c7b1
Jorengarenar <jakublukasiewicz@o2.pl>
2018-04-03 01:36:40 +0200

    2018-04-03_01:36:40

---
+++ linking.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+linking() {
+    if [ ! -e $2 ]; then
+        ln -sf $(pwd)/$1 $2
+    fi
+}
+
+cd $(dirname $0)
+
+# linking  terminator_config  $HOME/.config/terminator/config
+
+linking  bash_profile      $HOME/.bash_profile
+linking  bash_sources      $HOME/.bash_sources
+linking  bashrc            $HOME/.bashrc
+linking  dosbox-0.74.conf  $HOME/.dosbox/dosbox-0.74.conf
+linking  feh               $HOME/.config/feh/
+linking  tmux.conf         $HOME/.tmux.conf
+linking  vimrc             $HOME/.vimrc
+linking  Xresources        $HOME/.Xresources
+linking  zathura           $HOME/.config/zathura
+
+cd - > /dev/null
