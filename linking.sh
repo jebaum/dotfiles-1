43ee588decf4c778c77e87a9daaf5dbf4be21b82
Jorengarenar <jakublukasiewicz@o2.pl>
2018-04-07 18:02:37 +0200

    2018-04-07_18:02:37

--- linking.sh
+++ linking.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
+
 linking() {
-    if [ ! -e $2 ]; then
-        ln -sf $(pwd)/$1 $2
+    if [ $1 == "-f" -o $1 == "--force" ]; then
+        rm -rf $3
+    fi
+
+    if [ ! -e $3 ]; then
+        ln -sf $(pwd)/$2 $1
     fi
 }
 
+
+if [ ! $1 == "" ]; then
+    x=$1
+else
+    x="n"
+fi
+
+
 cd $(dirname $0)
 
-# linking  terminator_config  $HOME/.config/terminator/config
+# linking  $x  terminator_config  $HOME/.config/terminator/config
 
-linking  bash_profile      $HOME/.bash_profile
-linking  bash_sources      $HOME/.bash_sources
-linking  bashrc            $HOME/.bashrc
-linking  dosbox-0.74.conf  $HOME/.dosbox/dosbox-0.74.conf
-linking  feh               $HOME/.config/feh/
-linking  mpv/              $HOME/.config/mpv
-linking  tmux.conf         $HOME/.tmux.conf
-linking  vimrc             $HOME/.vimrc
-linking  Xresources        $HOME/.Xresources
-linking  zathura           $HOME/.config/zathura
+linking  $x  bash_profile       $HOME/.bash_profile
+linking  $x  bash_sources       $HOME/.bash_sources
+linking  $x  bashrc             $HOME/.bashrc
+linking  $x  dosbox-0.74.conf   $HOME/.dosbox/dosbox-0.74.conf
+linking  $x  feh                $HOME/.config/feh/
+linking  $x  mpv/               $HOME/.config/mpv
+linking  $x  tmux.conf          $HOME/.tmux.conf
+linking  $x  vimrc              $HOME/.vimrc
+linking  $x  Xresources         $HOME/.Xresources
+linking  $x  zathura            $HOME/.config/zathura
 
 cd - > /dev/null
