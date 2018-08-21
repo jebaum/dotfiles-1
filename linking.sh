960f75d8e5be51be3bd3c8533a1f871607f7d626
Jorengarenar <jakublukasiewicz@o2.pl>
2018-08-22 00:54:08 +0200

    2018-08-22_00:54:08

--- linking.sh
+++ linking.sh
@@ -7,6 +7,7 @@ linking() {
     fi
 
     if [ ! -e $3 ]; then
+        mkdir -p "$(dirname $3)"
         ln -sf $(pwd)/$2 $3
     fi
 }
@@ -21,19 +22,19 @@ fi
 
 cd $(dirname $0)
 
-linking  $x  bash_profile      $HOME/.bash_profile
-linking  $x  bash_sources      $HOME/.bash_sources
-linking  $x  bashrc            $HOME/.bashrc
+linking  $x  bash/bash_profile      $HOME/.bash_profile
+linking  $x  bash/bash_sources      $HOME/.bash_sources
+linking  $x  bash/bashrc            $HOME/.bashrc
 linking  $x  dosbox-0.74.conf  $HOME/.dosbox/dosbox-0.74.conf
 linking  $x  feh               $HOME/.config/feh
 linking  $x  inputrc           $HOME/.inputrc
 linking  $x  mpv/              $HOME/.config/mpv
 linking  $x  profile           $HOME/.profile
 linking  $x  tmux.conf         $HOME/.tmux.conf
-linking  $x  vimrc             $HOME/.vimrc
+linking  $x  vim/vimrc             $HOME/.vimrc
 linking  $x  Xresources        $HOME/.Xresources
 linking  $x  zathura           $HOME/.config/zathura
 
-# linking  $x  terminator_config  $HOME/.config/terminator/config
+# linking  $x  terminator $HOME/.config/terminator
 
 cd - > /dev/null
