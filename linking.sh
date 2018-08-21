8cdd5cfcee5805315c3ad01a7e02ae2df4eebe99
Jorengarenar <jakublukasiewicz@o2.pl>
2018-08-22 01:27:52 +0200

    2018-08-22_01:27:52

--- linking.sh
+++ linking.sh
@@ -19,22 +19,23 @@ else
     x="n"
 fi
 
-
 cd $(dirname $0)
 
-linking  $x  bash/bash_profile      $HOME/.bash_profile
-linking  $x  bash/bash_sources      $HOME/.bash_sources
-linking  $x  bash/bashrc            $HOME/.bashrc
-linking  $x  dosbox-0.74.conf  $HOME/.dosbox/dosbox-0.74.conf
-linking  $x  feh               $HOME/.config/feh
-linking  $x  inputrc           $HOME/.inputrc
-linking  $x  mpv/              $HOME/.config/mpv
-linking  $x  profile           $HOME/.profile
-linking  $x  tmux.conf         $HOME/.tmux.conf
-linking  $x  vim/vimrc             $HOME/.vimrc
-linking  $x  Xresources        $HOME/.Xresources
-linking  $x  zathura           $HOME/.config/zathura
+linking  $x  bash/bash_profile  $HOME/.bash_profile
+linking  $x  bash/bash_sources  $HOME/.bash_sources
+linking  $x  bash/bashrc        $HOME/.bashrc
+linking  $x  dosbox-0.74.conf   $HOME/.dosbox/dosbox-0.74.conf
+linking  $x  feh                $HOME/.config/feh
+linking  $x  inputrc            $HOME/.inputrc
+linking  $x  mpv/               $HOME/.config/mpv
+linking  $x  profile            $HOME/.profile
+linking  $x  tmux.conf          $HOME/.tmux.conf
+linking  $x  vim/vimrc          $HOME/.vimrc
+linking  $x  Xresources         $HOME/.Xresources
+linking  $x  zathura            $HOME/.config/zathura
 
 # linking  $x  terminator $HOME/.config/terminator
 
 cd - > /dev/null
+
+mkdir $HOME/.vimtmp
