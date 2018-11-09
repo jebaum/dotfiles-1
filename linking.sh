373dac3f28d89b3b9f013e780b074517cefdc743
Jorengarenar <jakublukasiewicz@o2.pl>
2018-11-09 17:53:14 +0100

    2018-11-09_17:53:14

--- linking.sh
+++ linking.sh
@@ -21,6 +21,8 @@ fi
 
 cd $(dirname $0)
 
+# linking  $x  rtv                $HOME/.config/rtv
+# linking  $x  terminator $HOME/.config/terminator
 linking  $x  bash/bash_profile  $HOME/.bash_profile
 linking  $x  bash/bash_sources  $HOME/.bash_sources
 linking  $x  bash/bashrc        $HOME/.bashrc
@@ -28,16 +30,14 @@ linking  $x  dosbox-0.74.conf   $HOME/.dosbox/dosbox-0.74.conf
 linking  $x  feh                $HOME/.config/feh
 linking  $x  inputrc            $HOME/.inputrc
 linking  $x  mpv/               $HOME/.config/mpv
-linking  $x  myclirc            $HOME/.myclirc
+linking  $x  mysql/my.cnf       $HOME/.my.cnf
+linking  $x  mysql/myclirc      $HOME/.myclirc
 linking  $x  profile            $HOME/.profile
 linking  $x  tmux.conf          $HOME/.tmux.conf
 linking  $x  vim/UltiSnips      $HOME/.vim/UltiSnips
 linking  $x  vim/vimrc          $HOME/.vimrc
 linking  $x  Xresources         $HOME/.Xresources
 linking  $x  zathura            $HOME/.config/zathura
-linking  $x  rtv                $HOME/.config/rtv
-
-# linking  $x  terminator $HOME/.config/terminator
 
 cd - > /dev/null
 
