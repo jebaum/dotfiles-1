feea43de009563f6b062bb7a4cdf7c4cfca73214
joren <jakublukasiewicz@o2.pl>
2019-05-18 02:50:12 +0200

    2019-05-18_02:50:12

--- linking.sh
+++ linking.sh
@@ -18,24 +18,24 @@ fi
 
 cd $(dirname $0)
 
-linking  $x  bash/bash_profile       $HOME/.bash_profile
-linking  $x  bash/inputrc            $HOME/.inputrc
-linking  $x  gtk/gtk3_settings.ini   $HOME/.config/gtk-3.0/settings.ini
-linking  $x  gtk/gtkrc-2.0           $HOME/.gtkrc-2.0
-linking  $x  i3/                     $HOME/.i3
-linking  $x  mpv/                    $HOME/.config/mpv
-linking  $x  mutt/muttrc             $HOME/.muttrc
-linking  $x  other/feh_keys          $HOME/.config/feh/keys
-linking  $x  other/gpg-agent.conf    $HOME/.gnupg/gpg-agent.conf
-linking  $x  other/profile           $HOME/.profile
-linking  $x  other/tmux.conf         $HOME/.tmux.conf
-linking  $x  other/Xresources        $HOME/.Xresources
-linking  $x  other/zathurarc         $HOME/.config/zathura/zathurarc
-linking  $x  vim/indent              $HOME/.vim/indent
-linking  $x  vim/UltiSnips           $HOME/.vim/UltiSnips
-linking  $x  vim/vimrc               $HOME/.vimrc
+linking  $x  gtk/gtk3_settings.ini  $HOME/.config/gtk-3.0/settings.ini
+linking  $x  gtk/gtkrc-2.0          $HOME/.gtkrc-2.0
+linking  $x  i3/                    $HOME/.i3
+linking  $x  mpv/                   $HOME/.config/mpv
+linking  $x  mutt/muttrc            $HOME/.muttrc
+linking  $x  other/bash_profile     $HOME/.bash_profile
+linking  $x  other/feh_keys         $HOME/.config/feh/keys
+linking  $x  other/gpg-agent.conf   $HOME/.gnupg/gpg-agent.conf
+linking  $x  other/inputrc          $HOME/.inputrc
+linking  $x  other/profile          $HOME/.profile
+linking  $x  other/tmux.conf        $HOME/.tmux.conf
+linking  $x  other/Xresources       $HOME/.Xresources
+linking  $x  other/zathurarc        $HOME/.config/zathura/zathurarc
+linking  $x  vim/indent             $HOME/.vim/indent
+linking  $x  vim/UltiSnips          $HOME/.vim/UltiSnips
+linking  $x  vim/vimrc              $HOME/.vimrc
 
-# linking  $x  other/myclirc           $HOME/.myclirc
+# linking  $x  other/myclirc          $HOME/.myclirc
 
 cd - > /dev/null
 
